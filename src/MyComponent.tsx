a48f3571588a24e646d5e01ccf7513e9163fbe2e
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-04-12 11:19:41 -0600

    style: Test.

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -11,7 +11,7 @@ const MyComponent = () => {
       <View style={tailwind('pt-12 items-center')}>
         <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
           <Text style={[tailwind('text-blue-800 font-semibold'), globalStyles.font20]}>
-            Hello Tailwind
+            Hello Tailwind, que tal
           </Text>
         </View>
       </View>
